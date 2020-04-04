2f081f8b0106ac6c20be1b6615581c108a104a0a
artgletsyan <artletsyan@gmail.com>
2020-04-04 18:52:42 +0400

    description add

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   pathPrefix: `/gatsby-creative`,
   siteMetadata: {
     title: `ReqSponse`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `We create awesome websites, the perfect solution for your project.`,
+    author: `reqsponse`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
