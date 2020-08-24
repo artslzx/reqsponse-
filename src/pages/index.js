b9238c7564e88578b193ef5ddbbcf812b50791c7
Art <artletsyan@gmail.com>
2020-08-24 22:47:46 +0400

    Change phonenumber

--- src/pages/index.js
+++ src/pages/index.js
@@ -201,7 +201,7 @@ export default class IndexPage extends React.Component {
             <div className="row">
               <div className="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
                 <i className="fas fa-phone fa-3x mb-3 text-muted"></i>
-                <div>+374 91 67-50-70</div>
+                <div>+374 -- -- --</div>
               </div>
               <div className="col-lg-4 mr-auto text-center">
                 <i className="fas fa-envelope fa-3x mb-3 text-muted"></i>
